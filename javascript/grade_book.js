3322e505520f7b8edc3bba62b45f7db453b0f9a4
Joao Tinti <joaotinti75@gmail.com>
2021-01-25 16:31:42 -0300

    Adicionando mais arquivos

---
+++ javascript/grade_book.js
@@ -0,0 +1,19 @@
+/*Complete the function so that it finds the mean of the three scores passed to it and returns the letter value associated with that grade.
+
+Numerical Score	Letter Grade
+90 <= score <= 100	'A'
+80 <= score < 90	'B'
+70 <= score < 80	'C'
+60 <= score < 70	'D'
+0 <= score < 60	'F'
+Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100. */
+
+function getGrade (s1, s2, s3) {
+    let mean = parseInt((s1 + s2 + s3) / 3)
+    
+    if (mean >= 90 && mean <= 100)    {return 'A'}
+    else if (mean >= 80 && mean < 90) {return 'B'}
+    else if (mean >= 70 && mean < 80) {return 'C'}
+    else if (mean >= 60 && mean < 70) {return 'D'}
+    else {return 'F'}
+}
